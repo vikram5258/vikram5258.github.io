948d24418b9d153e14cadce875da561a8906c8c1
vikram <vikram@thinkebiz.net>
2021-05-13 10:23:05 +0530

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 // import logo from './logo.svg';
 import "./App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Demo from "./components/Demo";
+import Resume from "./components/Resume";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World !!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/demos">Demos</Link>
+            </li>
+            <li>
+              <Link to="/resume">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <h1>Hello World !!</h1>
+        <Switch>
+          <Route path="/demos" component={Demo} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Demo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Demo() {
+  return <div>This is demo page</div>;
+}
+
+export default Demo;
