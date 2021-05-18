4706448ff30504bf5e2a96156e0d619e6b13355c
vikram <vikram@thinkebiz.net>
2021-05-18 10:20:45 +0530

    to build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  HashRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // import './index.css';
 import App from "./App";
